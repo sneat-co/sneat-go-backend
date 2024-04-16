399fbba34fddcb59e82d70bd4dd165a067924329
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2024-04-16 07:59:11 +0100

    chore(ci): use custom .git-hooks

---
+++ githooks_setup_test.go
@@ -0,0 +1,19 @@
+package sneatteamgo
+
+import "testing"
+import "os/exec"
+
+func TestGitHooksSetup(t *testing.T) {
+	// git config --local include.path ../.gitconfig
+	cmd := exec.Command("git", "config", "--local", "include.path", "../.gitconfig")
+	if cmd.Err == nil {
+		t.Log("Updated local git config to use .gitconfig")
+		return
+	}
+	output, err := cmd.Output()
+	if err == nil {
+		t.Errorf("Failed to update local git config: %s\nOutput:\n%s", cmd.Err, string(output))
+	} else {
+		t.Errorf("Failed to update local git config: %s\nFailed to read git command output: %s", cmd.Err, err)
+	}
+}
