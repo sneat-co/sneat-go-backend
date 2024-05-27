d722f3cffa219edc4a4cb6d4fe6ec52a6e93fa2a
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2024-05-27 22:27:29 +0100

    fix: small code formatting

--- src/modules/linkage/api4linkage/http_update_item_relationships.go
+++ src/modules/linkage/api4linkage/http_update_item_relationships.go
@@ -12,8 +12,8 @@ import (
 
 func httpUpdateItemRelationships(w http.ResponseWriter, r *http.Request) {
 	var request dto4linkage.UpdateItemRequest
-	handler := func(ctx context.Context, userCtx facade.User) (interface{}, error) {
-		return nil, facade4linkage.UpdateItemRelationships(ctx, userCtx, request)
-	}
-	apicore.HandleAuthenticatedRequestWithBody(w, r, &request, verify.DefaultJsonWithAuthRequired, http.StatusNoContent, handler)
+	apicore.HandleAuthenticatedRequestWithBody(w, r, &request, verify.DefaultJsonWithAuthRequired, http.StatusNoContent,
+		func(ctx context.Context, userCtx facade.User) (interface{}, error) {
+			return nil, facade4linkage.UpdateItemRelationships(ctx, userCtx, request)
+		})
 }
