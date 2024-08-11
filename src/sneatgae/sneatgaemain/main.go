a708310371380f4dcf367074017e6f0e8ece7912
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2024-08-11 08:15:49 +0100

    fix: serve static content in local dev env

--- src/sneatgae/sneatgaemain/main.go
+++ src/sneatgae/sneatgaemain/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"github.com/julienschmidt/httprouter"
 	"github.com/pkg/profile"
 	"github.com/sneat-co/sneat-go-backend/src/sneatgae/sneatgaeapp"
 	"github.com/sneat-co/sneat-go-core/emails/email2writer"
 	"github.com/strongo/logus"
 	"github.com/strongo/slice"
 	"io"
+	"net/http"
 	"os"
 )
 
@@ -20,8 +22,32 @@ func main() { // TODO: document why we need this wrapper
 
 	httpRouter := sneatgaeapp.CreateHttpRouter()
 
+	staticDir := http.Dir("static")
+	httpRouter.ServeFiles("/static/*filepath", staticDir)
+
+	serveRootStaticFiles(httpRouter, staticDir)
+
 	emailClient := email2writer.NewClient(func() (io.StringWriter, error) {
 		return os.Stdout, nil
 	})
 	sneatgaeapp.Start(nil, nil, httpRouter, emailClient)
+}
+
+func serveRootStaticFiles(httpRouter *httprouter.Router, root http.FileSystem) {
+	fileServer := http.FileServer(root)
+
+	for _, file := range []struct {
+		path        string
+		contentType string
+	}{
+		{path: "robots.txt", contentType: "text/plain"},
+		{path: "no-robots.txt", contentType: "text/plain"},
+		{path: "favicon.ico", contentType: "image/x-icon"},
+	} {
+		httpRouter.GET("/"+file.path, func(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+			w.Header().Set("Content-Type", file.contentType)
+			w.Header().Set("Cache-Control", "public, max-age=3600") // 1 hour (3600 seconds)
+			fileServer.ServeHTTP(w, req)
+		})
+	}
 }
