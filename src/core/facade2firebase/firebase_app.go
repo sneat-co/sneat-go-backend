5ef2283956fd310ef385ac8736b35fe57b057a46
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2024-09-08 10:20:31 +0100

    fix(auth): servcieaccoubt

--- src/core/facade2firebase/firebase_app.go
+++ src/core/facade2firebase/firebase_app.go
@@ -8,7 +8,7 @@ import (
 
 func GetFirebaseApp(ctx context.Context) (app *firebase.App, err error) {
 	conf := &firebase.Config{
-		ServiceAccountID: "my-client-id@my-project-id.iam.gserviceaccount.com",
+		//ServiceAccountID: "my-client-id@my-project-id.iam.gserviceaccount.com",
 	}
 	if app, err = firebase.NewApp(ctx, conf); err != nil {
 		err = fmt.Errorf("faield to initializing Firebase app: %w", err)
