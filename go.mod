a307c57b12b9abd442fced505d8e7e514a00de14
Alexander Trakhimenok <alex@trakhimenok.com>
2025-04-30 08:10:01 +0100

    fix(deps): latest core modules

--- go.mod
+++ go.mod
@@ -25,9 +25,9 @@ go 1.24.2
 require (
 	github.com/crediterra/money v0.3.0
 	github.com/dal-go/dalgo v0.18.2
-	github.com/dal-go/mocks4dalgo v0.2.5
+	github.com/dal-go/mocks4dalgo v0.2.6
 	github.com/julienschmidt/httprouter v1.3.0
-	github.com/sneat-co/sneat-core-modules v0.28.1
+	github.com/sneat-co/sneat-core-modules v0.28.2
 	github.com/sneat-co/sneat-go-core v0.50.13
 	github.com/stretchr/testify v1.10.0
 	github.com/strongo/decimal v0.1.1
--- src/modules/calendarium/module.go
+++ src/modules/calendarium/module.go
@@ -1,9 +1,8 @@
 package calendarium
 
 import (
-	"github.com/sneat-co/sneat-core-modules/linkage/dbo4linkage"
+	"github.com/sneat-co/sneat-core-modules/linkage/facade4linkage"
 	"github.com/sneat-co/sneat-core-modules/spaceus/dal4spaceus"
-	"github.com/sneat-co/sneat-core-modules/spaceus/facade4spaceus"
 	"github.com/sneat-co/sneat-go-backend/src/modules/calendarium/api4calendarium"
 	"github.com/sneat-co/sneat-go-backend/src/modules/calendarium/const4calendarium"
 	"github.com/sneat-co/sneat-go-backend/src/modules/calendarium/dbo4calendarium"
@@ -12,11 +11,15 @@ import (
 )
 
 func Module() module.Module {
-	facade4spaceus.RegisterDboFactory(const4calendarium.ModuleID, const4calendarium.HappeningsCollection,
-		func() (dal4spaceus.SpaceModuleDbo, dal4spaceus.SpaceItemDbo, *dbo4linkage.WithRelatedAndIDs) {
-			dbo := new(dbo4calendarium.HappeningDbo)
-			return new(dbo4calendarium.CalendariumSpaceDbo), dbo, nil
-		},
+	facade4linkage.RegisterDboFactory(const4calendarium.ModuleID, const4calendarium.HappeningsCollection,
+		facade4linkage.NewDboFactory(
+			func() facade4linkage.SpaceItemDboWithRelatedAndIDs {
+				return new(dbo4calendarium.HappeningDbo)
+			},
+			func() dal4spaceus.SpaceModuleDbo {
+				return new(dbo4calendarium.CalendariumSpaceDbo)
+			},
+		),
 	)
 	return module.NewModule(const4calendarium.ModuleID,
 		module.RegisterRoutes(api4calendarium.RegisterHttpRoutes),
